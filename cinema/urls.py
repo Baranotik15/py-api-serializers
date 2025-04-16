cdcfb0b8e7f4b8e078ff3f82b45f2f3f16a343a6
RuslanTopal <baranotik15@gmail.com>
2025-04-16 10:27:44 +0000

    add view url and start serializers

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,24 @@
-# write urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    ActorViewSet,
+    GenreViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+)
+
+
+router = DefaultRouter()
+router.register('cinema/actors', ActorViewSet)
+router.register('cinema/genres', GenreViewSet)
+router.register('cinema/cinema_halls', CinemaHallViewSet)
+router.register('cinema/movies', MovieViewSet)
+router.register('cinema/movie_sessions', MovieSessionViewSet)
+
+app_name = "cinema"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
